1c5488a79f6e230bcccdddfbd67e48a424084b8d
castelo494 <ansorisory494@gmail.com>
2022-10-26 23:12:42 +0800

    first commit

---
+++ database/migrations/2022_10_25_121429_create_produks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id('id_produk');
+            $table->string('nama');
+            $table->string('dekskripsi');
+            $table->string('harga');
+            $table->integer('stock');
+            $table->unsignedBigInteger('id_kategori'); 
+            $table->foreign('id_kategori')->references('id_kategori')->on('kategoris');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+};
